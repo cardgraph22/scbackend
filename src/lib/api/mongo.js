751dec9508742e947fd08740c1e1a8fde1cf39f5
cardgraph22 <129220366+cardgraph22@users.noreply.github.com>
2025-11-22 15:37:36 -0800

    Debug mongo

--- src/lib/api/mongo.js
+++ src/lib/api/mongo.js
@@ -10,7 +10,7 @@ let MONGODB_URI = process.env.MONGODB_URI;
 export async function startMongo() {
   console.log('mongo.js, connect to db')
   //mongoose.connect(MONGODB_URL)
-
+  console.log('mongo.js, try to connect')
   try {
     await mongoose.connect(MONGODB_URL)
     console.log('connected to mongoose')
