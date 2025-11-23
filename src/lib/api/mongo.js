0a4a184367c2c415d32ce51146bdbe47a65e690a
cardgraph22 <129220366+cardgraph22@users.noreply.github.com>
2025-11-22 16:05:15 -0800

    clean up mongo.js

--- src/lib/api/mongo.js
+++ src/lib/api/mongo.js
@@ -6,11 +6,9 @@ mongoose.set('strictQuery', false);
 
 //  use this for vercel
 let MONGODB_URI = process.env.MONGODB_URI;
-console.log('mongo.js, enter')
 export async function startMongo() {
-  console.log('mongo.js, connect to db')
+  //console.log('mongo.js, connect to db')
   //mongoose.connect(MONGODB_URL)
-  console.log('mongo.js, try to connect')
   try {
     await mongoose.connect(MONGODB_URI)
     console.log('connected to mongoose')
